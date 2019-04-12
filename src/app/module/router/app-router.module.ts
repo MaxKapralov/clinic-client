598c2b30bf636833d35cb0d625bb595d94bb1d1e
maxkapralov <maxkapralov997@gmail.com>
2019-04-12 19:21:46 +0200

    add styles

--- src/app/module/router/app-router.module.ts
+++ src/app/module/router/app-router.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from '../../component/login/login.component';
 import { MainComponent } from '../../component/main/main.component';
-import { RegistrationComponent } from '../../component/registration/registration.component';
 import { LoginPageComponent } from '../../component/login-page/login-page.component';
 
 
 const routes: Routes = [{ path: '', component: LoginPageComponent },
   { path: 'main', component: MainComponent },
-  { path: 'registration', component: RegistrationComponent}
   ];
 
 @NgModule({
