4feb8fe0a9c8098801bda568bda9be40714e8c29
A Strotska <alina.strotska@gmail.com>
2019-04-12 18:26:35 +0200

    css--->rozdzielone

--- src/app/module/router/app-router.module.ts
+++ src/app/module/router/app-router.module.ts
@@ -4,11 +4,12 @@ import { LoginComponent } from '../../component/login/login.component';
 import { MainComponent } from '../../component/main/main.component';
 import { RegistrationComponent } from '../../component/registration/registration.component';
 import { LoginPageComponent } from '../../component/login-page/login-page.component';
+import {RegistrationPageComponent} from '../../component/registration-page/registration-page.component';
 
 
 const routes: Routes = [{ path: '', component: LoginPageComponent },
   { path: 'main', component: MainComponent },
-  { path: 'registration', component: RegistrationComponent}
+  { path: 'registration', component: RegistrationPageComponent}
   ];
 
 @NgModule({
